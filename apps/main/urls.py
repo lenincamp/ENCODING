3fc7180cb9a09cf3c858c8be18861a474c4a736f
lenincamp <lenin@MacBook-Pro-de-Lenin.local>
2015-03-30 13:11:23 -0500

    cambios con login
    se agrego el login en un template aparte

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -3,7 +3,7 @@ from .views import *
 from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name="main/home.html"), name='main_none_none'),
+    url(r'^$', TemplateView.as_view(template_name="main/login.html"), name='main_none_none'),
     url(r'^login/$', login.as_view(), name='main_usuario_login'),
-    #url(r'^$', TemplateView.as_view(template_name="main/logged_user.html"), name='main_none_none'),
+    url(r'^main/logged_user/$', TemplateView.as_view(template_name="main/logged_user.html"), name='main_none_none'),
 )
